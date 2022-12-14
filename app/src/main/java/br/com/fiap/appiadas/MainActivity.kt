309887262cf45156ed783a88bfff5fe379024da6
Danilo Santos <danilopsnts@gmail.com>
2022-12-13 22:11:44 -0300

    add jokers

--- app/src/main/java/br/com/fiap/appiadas/MainActivity.kt
+++ app/src/main/java/br/com/fiap/appiadas/MainActivity.kt
@@ -2,10 +2,26 @@ package br.com.fiap.appiadas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.fiap.appiadas.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btTellJoker.setOnClickListener {
+            showJoker()
+        }
+    }
+
+    private fun showJoker() {
+        val jokers = resources.getStringArray( R.array.jokers)
+        val numberJoker = Random().nextInt( jokers.size)
+        val joker = jokers[numberJoker ]
+        binding.tvJoker.text = joker
     }
 }
